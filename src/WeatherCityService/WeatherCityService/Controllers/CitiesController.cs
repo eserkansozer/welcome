9143abae63231605bf81182d3add3fe2e0792c1a
Serkan Sozer <eserkansozer@hotmail.com>
2018-10-17 17:43:52 +0100

    Adding core web service for city list

---
+++ src/WeatherCityService/WeatherCityService/Controllers/CitiesController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace WeatherCityService.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Cities")]
+    public class CitiesController : Controller
+    {
+        // GET: api/Cities
+        [HttpGet]
+        public JArray Get()
+        {
+            var json = System.IO.File.ReadAllText("Data/city-list.json");
+            var list = JArray.Parse(json);
+            return list;
+        }
+
+        //// GET: api/Cities/5
+        //[HttpGet("{id}", Name = "Get")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+        
+        //// POST: api/Cities
+        //[HttpPost]
+        //public void Post([FromBody]string value)
+        //{
+        //}
+        
+        //// PUT: api/Cities/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
+        
+        //// DELETE: api/ApiWithActions/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
--- src/app/Services/weather-city.service.ts
+++ src/app/Services/weather-city.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
 import { DataApiService } from './data-api.service';
 import { Http } from '@angular/http';
+import { environment } from 'environments/environment';
 
 @Injectable()
 export class WeatherCityService extends DataApiService{
 
   constructor(http: Http) {
-    super('assets/city-list.json', http);
+    super(environment.wcServiceUrl, http);
    }
 }
