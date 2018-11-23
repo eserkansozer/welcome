2cd313ec0ee59ebc8e7e8fc2e3e8aa687e13f8da
Serkan Sozer <eserkansozer@hotmail.com>
2018-11-23 17:53:06 +0000

    Restructure folders

---
+++ SPA/src/app/Services/daytime.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { TimeOfDay } from '../Enums';
+
+@Injectable()
+export class DaytimeService {
+
+  constructor() { }
+
+  getDayTime(): Number {
+
+    const NOON_TIME: Number = 12;
+    const DUSK_TIME: Number = 19;
+    const hours: Number = new Date().getHours() as Number;
+
+    if (hours < NOON_TIME) {
+      return TimeOfDay.Morning;
+    } else if (hours >= NOON_TIME && hours <= DUSK_TIME) {
+      return TimeOfDay.Afternoon;
+    } else {
+      return TimeOfDay.Evening;
+    }
+  }
+
+  getDate(): String {
+    return new Date().toISOString().split('T')[0];
+  }
+}
